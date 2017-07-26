39642299ef35ae539f14debc086ca61f4e0f5d3d
r3x1r <cool.priv@gmail.com>
2017-07-26 02:07:00 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,110 @@
+
+<script>alert('ACCESS DENIED !');</script>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>DARKNETHOST - #1 Faster Checker Services</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link href="assets/css/bootstrap.hack" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+ <style>
+    .tall-row {
+        margin-top: 40px;
+    }
+    .modal {
+        position: relative;
+        top: auto;
+        right: auto;
+        left: auto;
+        bottom: auto;
+        z-index: 1;
+        display: block;
+    }
+	body{
+  background: #222;
+  color: white;
+}
+
+div.well{
+  height: 250px;
+} 
+
+.Absolute-Center {
+  margin: auto;
+  position: absolute;
+  top: 0; left: 0; bottom: 0; right: 0;
+}
+
+.Absolute-Center.is-Responsive {
+  width: 50%; 
+  height: 50%;
+  min-width: 200px;
+  max-width: 600px;
+  padding: 40px;
+}
+
+.darknethost{
+		margin:0 auto;
+		float:none;
+	}
+	#mailist{
+		color:white;
+	}
+	#valid{
+		color:lime;
+	}
+	#limit{
+		color:orange;
+	}
+	#die{
+		color:red;
+	}
+	#recheck{
+		color:cyan;
+	}
+	
+	textarea{
+		padding-top:20px !important;
+		padding-bottom:20px !important;
+		resize:none;
+		background:#222 !important;
+		border:1px solid #586e75 !important;
+		
+	}
+	*{
+		border-radius:0px !important;
+	}
+	.label-status{
+		position:absolute;
+		left:15px;
+		
+	}
+	.label-hitung{
+		background:white;
+		color:#080808;
+		position:absolute;
+		right:15px;
+	}
+    </style>
+  </head>
+  <body>
+  <div class="container">
+  <div class="row">
+    <div class="Absolute-Center is-Responsive">
+      <center><h1 style="color:green"><i class="fa fa-bug"></i> <b>DARKNETHOST</b></h1></center>
+        <form action="" method="post" id="loginForm">
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="fa fa-hashtag"></i></span>
+            <input class="form-control" type="text" name='access_code' placeholder="DNHOST-098F6BCD4621D373CADE4E832627B4F60"/> 	
+          </div>
+          <div class="form-group" >
+            <button type="submit" class="btn btn-success btn-block">Login</button>
+          </div>
+        </form>         
+    </div>    
+  </div>
+</div>
+   </body>
+   </html>
